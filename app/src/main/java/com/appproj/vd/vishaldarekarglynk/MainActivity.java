e7b6cb311d3575ec0cbb94c737c1c133c670dba8
Vishal <vish.darekar@gmail.com>
2018-01-12 10:15:27 +0530

    New Font added. New Images to Tabbar

--- app/src/main/java/com/appproj/vd/vishaldarekarglynk/MainActivity.java
+++ app/src/main/java/com/appproj/vd/vishaldarekarglynk/MainActivity.java
@@ -1,10 +1,16 @@
 package com.appproj.vd.vishaldarekarglynk;
 
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         mToolbar.setTitle("");
         setViewPager();
 
+
     }
 
     private void setViewPager() {
@@ -32,5 +39,24 @@ public class MainActivity extends AppCompatActivity {
 
         mTabLayout = (TabLayout) findViewById(R.id.tab);
         mTabLayout.setupWithViewPager(mViewPager);
+        changeTabsFont();
+    }
+    private void changeTabsFont() {
+        Typeface typeface = Typeface.createFromAsset(getAssets(),
+                "Capture_it.ttf");
+        ViewGroup vg = (ViewGroup) mTabLayout.getChildAt(0);
+        int tabsCount = vg.getChildCount();
+        for (int j = 0; j < tabsCount; j++) {
+            ViewGroup vgTab = (ViewGroup) vg.getChildAt(j);
+            int tabChildsCount = vgTab.getChildCount();
+            for (int i = 0; i < tabChildsCount; i++) {
+                View tabViewChild = vgTab.getChildAt(i);
+                if (tabViewChild instanceof TextView) {
+                    ((TextView) tabViewChild).setTypeface(typeface);
+
+
+                }
+            }
+        }
     }
 }
